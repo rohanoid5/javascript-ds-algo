6d0a5bb3d71d2f5ca556d56a84fbbd7bdad04531
rohanoid5 <rohanoid5@gmail.com>
2022-08-02 00:09:09 +0530

    Added Description for Djikstra's

--- src/algorithms/graph/djikstra/SingleSourceShortestPath.js
+++ src/algorithms/graph/djikstra/SingleSourceShortestPath.js
@@ -1,21 +1,19 @@
-const findMinDistance = function (dist, sptSet) {
-  let min = Number.MAX_SAFE_INTEGER;
-  let minIdx = -1;
-
-  for (let i = 0; i < dist.length; i++) {
-    if (!sptSet[i] && dist[i] <= min) {
-      min = dist[i];
-      minIdx = i;
-    }
-  }
-
-  return minIdx;
-};
-
 /**
  * Dijkstra’s shortest path algorithm
  * Given a graph and a source vertex in the graph,
  * find the shortest paths from the source to all vertices in the given graph.
+ * Only works for graph with no negative weight cycles.
+ *
+ * Solution: T = O(V^2), S = O(V)
+ * 1. Create an array of distance (dist) from source and initialize it with INT.MAX
+ * 2. Create a set for all the vertices (SptSet)
+ * 3. Initialize the SptSet with source and make source distance as 0 in dist
+ * 4. Start iterating over all the vertices.
+ * 5. Pick the vertex (u) with minimum distance which is not in SptSet
+ * 6. Check all the adjacent vertices (v) of that vertex
+ * 7. If v is not in SptSet and dist[v] > dist[u] + Weight of Edge between u and v
+ * 8. Update dist[v] as dist[u] + Weight of Edge between u and v
+ * 9. dist will be the shortest path of all vertices from source
  */
 const singleSourceShortestPath = function (graph, source) {
   const { adjacencyMatrix, weights } = graph;
@@ -45,6 +43,20 @@ const singleSourceShortestPath = function (graph, source) {
   }
 
   return dist;
+};
+
+const findMinDistance = function (dist, sptSet) {
+  let min = Number.MAX_SAFE_INTEGER;
+  let minIdx = -1;
+
+  for (let i = 0; i < dist.length; i++) {
+    if (!sptSet[i] && dist[i] <= min) {
+      min = dist[i];
+      minIdx = i;
+    }
+  }
+
+  return minIdx;
 };
 
 module.exports = singleSourceShortestPath;
